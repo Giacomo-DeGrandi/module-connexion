d4bcf79896e437ecb79b4d12ba44a1bfec48665f
Giacomo-DeGrandi <carlo.de-grandi-giacomo@laplateforme.io>
2021-11-21 12:16:45 +0100

    j'ai un souci sur inscription, parce-que en rechargeant la page, si j'avais envoyé une requete sql il reinvoi la meme requete. le souci c'est que meme si j'ai posé des condition pour ne pas avoir deux nom de login pareil je me retrouve à les avoir.. j'essaie de poser une variable de session pour m y referir et bloquer la requete si egal au login name

--- pages/connexion.php
+++ pages/connexion.php
@@ -31,29 +31,44 @@ $conn = mysqli_connect($servername, $username, $password, $database);	// establi
 
 $quest = "SELECT login, password FROM utilisateurs "; 
 
-$req = mysqli_query($conn,$quest);
+		$req = mysqli_query($conn,$quest);
 
-$res = mysqli_fetch_all($req, MYSQLI_ASSOC);
+		$res = mysqli_fetch_all($req, MYSQLI_ASSOC);
+
+		foreach($res as $v){
+			if($v == $_POST["login"] and $v == $_POST["password"]){
+
+				echo $v;
+
 
 
 if (	(isset($_POST["login"])) && $_POST['login'] != ''){
 	if (	(isset($_POST["password"])) && $_POST['password'] != '') {
 
-		$_SESSION["login"][] = $_POST["login"];
 
-		$_SESSION["password"][] = $_POST["password"];
 
-		foreach($res as $v){
-			if($v == $_POST["login"] or $v == $_POST["password"]){
-				$_SESSION["login"]++;
-				$_SESSION["password"]++;
+
+			//	$_SESSION["login"][] = $_POST["login"];
+
+			//	$_SESSION["password"][] = $_POST["password"];
+
+			//	if($v === 'admin'){
+			//		header( "Location: admin.php" );
+			//	}
+
+			//	echo 'HELLO' .'<h1>'. $_POST["login"] .'</h1>'.'YOU\'RE NOW LOGGED IN';
+
+			//	echo '<br><a href="profil.php"><input type="submit" value="profile" name="profile></a>';
+
+			//		$_SESSION["login"]++;
+			//		$_SESSION["password"]++;
+
+			//	if (isset($_POST['submit'])){
+
+			//	header( "Location: profil.php" );
+			//	}
 			}
 		} 
-		
-		if (isset($_POST['submit'])){
-
-			header( "Location: profil.php" );
-		}
 	}
 }
 
--- pages/inscription.php
+++ pages/inscription.php
@@ -1,5 +1,8 @@
 <?php
 
+
+// Connection to database 
+
 			$servername = 'localhost';
 			$username = 'root';
 			$password = '';
@@ -8,31 +11,49 @@
 			$conn = mysqli_connect($servername, $username, $password, $database);	// establish my connexion
 
 
+// Pose my conditions to validate forms if all fields are filled
 
-if( (isset($_POST['login'])&& ($_POST['login']) != '') &&
-	 	(isset($_POST['prenom'])&& ($_POST['prenom']) != '') &&
-	 	(isset($_POST['nom'])&& ($_POST['nom']) != '') &&
-	 	(isset($_POST['password'])&& ($_POST['password']) != '')){
+			// I start with login to check if it already exist , if it exists, i send an error message
+			// like this i don't have to stay checking the connection form and relate it to the id but is sufficient the login name 
 
-			$login = $_POST['login'];
-			$prenom = $_POST['prenom'];
-			$nom = $_POST['nom']; 
-			$password = $_POST['password'];
+if (isset($_POST['login'])&& ($_POST['login']) != '') { 
 
-			$quest= " INSERT INTO utilisateurs( login, prenom, nom, password) VALUES ('$login','$prenom','$nom','$password') ";
+
+			$quest = "SELECT login FROM utilisateurs "; 
 
 			$req = mysqli_query($conn,$quest);
 
-			if(isset($_POST['submit'])){
-			
-			header( "Location: connexion.php" );
+			$res = mysqli_fetch_all($req, MYSQLI_ASSOC);
 
+			for($i=0; $i<isset($res[$i]); $i++){
+				foreach($res[$i] as $k => $v){		
+					if($v !== $_POST['login']){
+
+								// Here i continue posing my conditions
+
+						if  (   (isset($_POST['prenom'])&& ($_POST['prenom']) != '') &&
+								 	(isset($_POST['nom'])&& ($_POST['nom']) != '') &&
+								 	(isset($_POST['password'])&& ($_POST['password']) != '')	)	{	//**
+
+								$login = $_POST['login'];
+								$prenom = $_POST['prenom'];
+								$nom = $_POST['nom']; 
+								$password = $_POST['password'];
+
+								$quest2= " INSERT INTO utilisateurs( login, prenom, nom, password) VALUES ('$login','$prenom','$nom','$password') ";
+
+								$req2 = mysqli_query($conn,$quest2);
+
+								if(isset($_POST['submit'])){
+								
+									header( "Location: connexion.php" );
+
+								}	
+						}	else { 	echo 'error . all fields are required';	}						//**isset($_POST['pass.	
+					}	else {	echo 'error . log in name alreasy exists';	}							//**if($v !== $_POST['l..
+				}
 			}
-
-	} else {
-
-		echo 'All fields are required*';
-	}
+} 
 
 
 ?>
@@ -50,7 +71,8 @@ if( (isset($_POST['login'])&& ($_POST['login']) != '') &&
 <header>
 </header>
 <main>
-	<form action='' method='post'>		
+	<h1>SIGN UP</h1>
+	<form action='' method='post'>	
 		<input type="text" name="login" placeholder="login" ><br>
 		<input type="text" name="prenom" placeholder="prenom"><br>
 		<input type="text" name="nom" placeholder="nom" ><br>
@@ -60,6 +82,8 @@ if( (isset($_POST['login'])&& ($_POST['login']) != '') &&
 	</form>
 	<br>
 	<a href="../index.php" target="_top">go back to the home page </a>
+	<br>
+	<a href="connexion.php" target="_top">Already Signed Up? Log in here </a>
 </main>
 </body>
 </html>
