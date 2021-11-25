aec2f5698ab47448be3888dcf26339003f364882
Giacomo-DeGrandi <carlo.de-grandi-giacomo@laplateforme.io>
2021-11-25 15:47:12 +0100

    commit typing error

--- index.php
+++ index.php
@@ -38,14 +38,14 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 
 /* fetch associative array */
 while ($row = mysqli_fetch_row($req)) {
-	if($row[1] === 0){
+	if($row[1] == 0){
 			
 			echo '<tr>';
 			echo '<td>'.$row[0].'</td>';			
 			echo '<td>'.' 🔴'.'</td>';
 			echo '</tr>';
 
-	} elseif ($row[1] === 1){
+	} elseif ($row[1] == 1){
 
 			echo '<tr>';
 			echo '<td>'.$row[0].'</td>';			
