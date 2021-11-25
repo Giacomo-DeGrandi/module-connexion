e7a762173c0f03f439f1fac2aaf6e778824e88cd
Giacomo-DeGrandi <carlo.de-grandi-giacomo@laplateforme.io>
2021-11-25 07:46:49 +0100

    changed path for plesk

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ session_start();
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="pages/modcon.css" rel="stylesheet"> 
+	<link href="modcon.css" rel="stylesheet"> 
 </head>
 <body>
 <header>
